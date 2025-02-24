d57cbc73833b2cf72569f78867e1e13013d30a19
piseth21 <chhengpiseth874@gmail.com>
2025-02-24 07:58:01 +0700

    first commit

---
+++ Port.js
@@ -0,0 +1,12 @@
+{
+  /* <script>
+  document.querySelector(".contact-btn").addEventListener("click", function () {
+    let contactSection = document.getElementById("Contact");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    } else {
+      console.error("Contact section not found!");
+    }
+  });
+</script> */
+}
